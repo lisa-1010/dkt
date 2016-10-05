fc26c6d9030faaf4acf5cdf68a5e1c49d6800f7e
Lisa Wang <lisa1010@stanford.edu>
2016-10-04 23:20:33 -0700

    added models and training script

--- code/tf_utils.py
+++ code/tf_utils.py
@@ -2,6 +2,8 @@
 
 import tensorflow as tf
 import tflearn
+import datetime
+
 
 def build_char_level_ast_dynamic_rnn_encoder(maxlen=100, vocab_size=50, embedding_size=64, hidden_dim=128):
     """
@@ -29,7 +31,7 @@ def build_char_level_ast_dynamic_rnn_encoder(maxlen=100, vocab_size=50, embeddin
     net = tflearn.lstm(net, hidden_dim, dropout=0.8, dynamic=True, return_seq=False)
     return net
 
-def build_rnn_from_ast_encoder_to_success(net):
+def build_rnn_from_ast_encoder_to_student_success(net):
     # incoming tensor of shape [None, max_seq_len, hidden_dim]
     y_pred = tflearn.time_distributed(net, tflearn.fully_connected, [2,'softmax'])
     # outgoing tensor of shape [None, max_seq_len, 2]
@@ -61,15 +63,35 @@ def build_lstm_net(n_timesteps, n_inputdim, n_hidden=128, n_outputdim):
     net = tflearn.layers.input_data([None, n_timesteps, n_inputdim])
     net = tflearn.layers.lstm(net, n_hidden, return_seq=True)
     net = tflearn.layers.dropout(net, 0.5)
-    # net = tflearn.layers.lstm(net, n_hidden, return_seq=True)
-    # net = tflearn.layers.dropout(net, 0.5)
-    # output from prev layer is a
-    net = tflearn.layers.fully_connected(net, 10, activation='softmax')
-    net = tflearn.layers.regression(net, optimizer='adam', learning_rate=0.001, loss='categorical_crossentropy')
+    y_pred = tflearn.time_distributed(net, tflearn.fully_connected, [2, 'softmax'])
+    # outgoing tensor of shape [None, max_seq_len, 2]
+    y_pred = tflearn.flatten(y_pred)  # make sure to also flatten y_true
+    net = tflearn.regression(y_pred, optimizer='adam', loss='binary_crossentropy')
+    return net
 
 
 
 def load_model(model_id, load_ckpt=False):
     net = build_lstm_net()
 
-    pass
+    model = None
+    return model
+
+
+def train_model(model_id, dataset_name, use_checkpoint=True):
+
+    x, y = load_data(dataset=dataset_name)
+
+    print ('x shape: {}'.format(x.shape))
+    print ('y shape: {}'.format(y.shape))
+
+    # check_model_exists_or_create_new(model_id, n_timesteps, n_inputdim, n_hidden, n_classes, architecture)
+
+    model = load_model(model_id, load_checkpoint=use_checkpoint, training_mode=True)
+    print ("Model loaded.")
+
+    date_time_string = datetime.datetime.now().strftime("%m-%d-%Y_%H-%M-%S")
+    run_id = "{}_{}".format(model_id, date_time_string)
+    print ("Run id: {}".format(run_id))
+
+    model.fit(x, y, n_epoch=32, validation_set=0.1, show_metric=True, snapshot_step=100, run_id=run_id)
